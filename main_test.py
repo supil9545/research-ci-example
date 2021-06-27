bfc799d627e7de1c5eba9a9978814cfed54483fc
supil <chmk95456@gmail.com>
2021-06-27 17:00:43 +0900

    add unittest

---
+++ main_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+import main
+
+
+class MainTest(unittest.TestCase):
+    def test_hellowworld(self):
+        ret = main.helloworld("Test")
+        self.assertEqual(ret, "Hello World! Chris!")
+
+if __name__ == "__main__":
+    unittest.main()
